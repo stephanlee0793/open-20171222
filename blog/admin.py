5579e9f7e304360531d24e3209e582467a8a7e8c
AskDjango <me@nomade.kr>
2017-12-22 06:31:18 +0000

    admin 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,10 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'get_length', 'created_at', 'updated_at']
+    list_filter = ['created_at']
+    search_fields = ['title']
+    
+    def get_length(self, post):
+        return len(post.content)
+    get_length.short_description = '내용 글자수'
