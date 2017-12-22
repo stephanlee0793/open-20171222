ef730984433c86c5bc74c39aada176bc765877fc
AskDjango <me@nomade.kr>
2017-12-22 07:11:13 +0000

    post_list 뷰 구현

--- askdjango20171222/urls.py
+++ askdjango20171222/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from blog.views import post_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', post_list),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    qs = Post.objects.all().order_by('-id')
+    # blog/templates/blog/post_list.html 생성
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
